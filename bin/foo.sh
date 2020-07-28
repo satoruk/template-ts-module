7be25a1917eafad6b3ffb237245f7f1a7e2068d3
satoruk <koyanagi3106@gmail.com>
2020-07-28 09:47:39 +0900

    WIP

---
+++ bin/foo.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "bin/foo.sh:3:5: use emoticon :+1:"
+echo "bin/foo.sh:4:5: use markdown `code` [Google](https://www.google.co.jp/)"
+echo "bin/foo.sh:5:5: use html <details><summary>spoiler</summary>Foo</details>"
